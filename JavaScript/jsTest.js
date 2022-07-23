02c171847b46c11cbd8a97ddf3a9fe7605768f5e
Officer_Choi <me940728@naver.com>
2022-07-23 12:30:56 +0900

    [UPDATE] start Study

--- JavaScript/jsTest.js
+++ JavaScript/jsTest.js
@@ -1,1 +1,4 @@
 'use strict'
+// NVM 설치 먼저 한다 NVM은 Nodejs Version Manager 로 노드를 설치하고 사용할 버전을 쉽게 전환하도록 도와주는 ShellScript다
+// NVM 설치후 nvm ls-remote 로 버전을 설치 가능한 버전 확인 후 nvm install 16.0.13 식으로 버전 설치를 한다 
+console.log("testLog");
---
+++ java_programming/src/javaStudy/testClass.java
@@ -0,0 +1,8 @@
+package javaStudy;
+
+public class testClass {
+	public static void main(String[] args){
+		System.out.println("Hello");
+	}
+
+}
