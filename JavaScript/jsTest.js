d44cfac0994cb8431298970c8197f1c92b94c120
Officer_Choi <me940728@naver.com>
2022-07-27 20:45:50 +0900

    [UPDATE] jsTest.js jsBackTick Test

--- JavaScript/jsTest.js
+++ JavaScript/jsTest.js
@@ -1,4 +1,7 @@
 'use strict'
 // NVM 설치 먼저 한다 NVM은 Nodejs Version Manager 로 노드를 설치하고 사용할 버전을 쉽게 전환하도록 도와주는 ShellScript다
 // NVM 설치후 nvm ls-remote 로 버전을 설치 가능한 버전 확인 후 nvm install 16.0.13 식으로 버전 설치를 한다 
+const tNum= 'backTick';
+
 console.log("testLog");
+console.log(`${tNum}`); // => 백틱은 영어 상태에서 ₩를 누르면 입력 가능함
