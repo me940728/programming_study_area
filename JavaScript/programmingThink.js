823a71ce38e224032e9d4dfaa84dbbc0f75cec67
Officer_Choi <me940728@naver.com>
2022-09-10 08:00:50 +0900

    [UPDATE] modifi js

--- JavaScript/programmingThink.js
+++ JavaScript/programmingThink.js
@@ -1,1 +1,21 @@
 'use strict'
+
+/*
+[Study-list]
+9월 
+    1. JPA -> 김영한님 책으로(구매 예정)
+    2. TDD -> TDD 책 (구입 완료)
+    3. JPA, TDD 적용한 demo RestAPI Project(sprint)
+    4. Swagger Spring-doc 활용한 demo Project 명세서 만들기
+        (참고 문서 : https://springdoc.org/#properties)
+    5. 프로젝트 팀 구성
+    6. demo 프로젝트 공모전 내기(API -> Android)
+10월
+    1. 하드웨어 지식 정리 -> 조직응용기사 시험
+    2. Java 11 문법 이펙티브하게 공부 -> OCJP11??
+    3. 디자인패턴 
+    4. data architecture -> 가이드(구매예정)
+11월 ~ 23년 7월
+    1. DAP
+    2. Demo project refactoring ~
+    */
